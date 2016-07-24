bbfe34ffe296f19e084cc628660fb007ca04a9b9
Tyler Fallon <tylerfallon@Tylers-MacBook-Pro.local>
2016-07-24 12:55:33 -0400

    made csv file and imported table data

---
+++ moresongs.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  Item_ID INT NOT NULL AUTO_INCREMENT,
+  ProductName VARCHAR(100) NOT NULL,
+  DepartmentName VARCHAR(100) NOT NULL,
+  Price DECIMAL(10,4) NULL,
+  StockQuantity INT NULL,
+  PRIMARY KEY (Item_ID)
+);
